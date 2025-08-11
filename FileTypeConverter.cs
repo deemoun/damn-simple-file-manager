a3c28648061bc0e2d17ded8e6b8c4e1516761420
Dmitry Yarygin <deemoun@users.noreply.github.com>
2025-08-11 14:38:14 +0200

    Show file type indicator before file names

---
+++ FileTypeConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace DamnSimpleFileManager
+{
+    /// <summary>
+    /// Converts FileSystemInfo to a string representing either a folder marker or file extension.
+    /// </summary>
+    public class FileTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DirectoryInfo)
+            {
+                return "📁"; // folder icon
+            }
+            if (value is FileInfo fi)
+            {
+                return string.IsNullOrEmpty(fi.Extension) ? "FILE" : fi.Extension.TrimStart('.').ToUpperInvariant();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
