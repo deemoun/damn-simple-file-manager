a643f7b580d4910ab01a1298fe599ef7803a9190
Dmitry Yarygin <deemoun@users.noreply.github.com>
2025-08-13 01:20:27 +0200

    Assert non-null language names

--- Localization.cs
+++ Localization.cs
@@ -69,6 +69,7 @@ namespace DamnSimpleFileManager
             return Directory.GetFiles(dir, "*.lang")
                 .Select(Path.GetFileNameWithoutExtension)
                 .Where(n => !string.IsNullOrWhiteSpace(n))
+                .Select(n => n!)
                 .ToHashSet(StringComparer.OrdinalIgnoreCase);
         }
     }
