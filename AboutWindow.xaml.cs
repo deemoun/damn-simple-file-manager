832747ad069379bdc5830a0a40734eab855260ae
Dmitry Yarygin <deemoun@users.noreply.github.com>
2025-08-13 01:44:04 +0200

    feat: add GitHub link in About window

--- AboutWindow.xaml.cs
+++ AboutWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace DamnSimpleFileManager
 {
@@ -12,6 +14,12 @@ namespace DamnSimpleFileManager
         private void Close_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
