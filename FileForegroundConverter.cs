6de446b75b6e259f63a8e02b53374750873dca2e
Dmitry Yarygin <deemoun@users.noreply.github.com>
2025-08-13 00:31:54 +0200

    Color only folder icons

---
+++ FileForegroundConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DamnSimpleFileManager
+{
+    /// <summary>
+    /// Provides foreground brush based on the file system entry type.
+    /// </summary>
+    public class FileForegroundConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is DirectoryInfo || value is ParentDirectoryInfo)
+            {
+                return Brushes.Yellow;
+            }
+            return SystemColors.ControlTextBrush;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
