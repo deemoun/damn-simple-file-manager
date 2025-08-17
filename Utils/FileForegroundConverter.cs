c1892a3e3bf1c886f2f92fa292879d8a088c1dff
Dmitry Yarygin <deemoun@users.noreply.github.com>
2025-08-17 18:45:49 +0200

    Handle missing convert-back operations

--- Utils/FileForegroundConverter.cs
+++ Utils/FileForegroundConverter.cs
@@ -23,7 +23,7 @@ namespace DamnSimpleFileManager
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
